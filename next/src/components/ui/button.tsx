482802d4bb9b8a890593c8ef81a8b1437af2a0ee
FelineJTD <13520050@std.stei.itb.ac.id>
2024-06-09 14:39:35 +0700

    feat: ui

---
+++ next/src/components/ui/button.tsx
@@ -0,0 +1,39 @@
+import { IconContext } from "@phosphor-icons/react";
+
+type ButtonProps = {
+  children: React.ReactNode;
+  variant?: "primary" | "secondary";
+  className?: string;
+  icon?: React.ReactNode;
+} & React.ButtonHTMLAttributes<HTMLButtonElement>;
+
+export default function Button({
+  children,
+  variant = "primary",
+  className,
+  icon,
+  ...props
+}: ButtonProps) {
+  return (
+    <IconContext.Provider
+      value={{
+        color: "currentColor",
+        size: 20,
+        weight: "bold",
+        mirrored: false
+      }}
+    >
+      <button
+        className={`py-3 px-6 rounded-full uppercase flex items-center ${
+          variant === "primary"
+            ? "bg-primary text-neutral-900 enabled:hover:bg-neutral-700 enabled:hover:text-primary duration-200"
+            : "text-primary border border-primary bg-transparent enabled:hover:bg-primary enabled:hover:text-neutral-900 duration-200"
+        } ${className}`}
+        {...props}
+      >
+        {icon && <span className="mr-2">{icon}</span>}
+        {children}
+      </button>
+    </IconContext.Provider>
+  );
+}
--- next/tailwind.config.ts
+++ next/tailwind.config.ts
@@ -4,17 +4,27 @@ const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    colors: {
+      transparent: "transparent",
+      primary: "#FFFF20",
+      neutral: {
+        900: "#101010",
+        800: "#2A2A2A",
+        700: "#434343",
+        600: "#5C5C5C",
+        500: "#757575",
+        400: "#8F8F8F",
+        300: "#A8A8A8",
+        200: "#C2C2C2",
+        100: "#DBDBDB",
+        50: "#F5F5F5"
+      }
     },
+    extend: {}
   },
-  plugins: [],
+  plugins: []
 };
 export default config;
